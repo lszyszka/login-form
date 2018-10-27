c4c9fbd5f4dd8cc4ff4e9085157c8d90acb10068
lszyszka <l.szyszka90@gmail.com>
2018-10-27 10:34:41 +0200

    Updates

---
+++ precache-manifest.511f871bdc2a239a9d83189fbec20535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91889155e2152d45bad0",
+    "url": "/lszyszka/login-form.git/static/js/runtime~main.91889155.js"
+  },
+  {
+    "revision": "5de4790c58a8e0d69a96",
+    "url": "/lszyszka/login-form.git/static/js/main.5de4790c.chunk.js"
+  },
+  {
+    "revision": "f0f5481770c73a2bebdc",
+    "url": "/lszyszka/login-form.git/static/js/1.f0f54817.chunk.js"
+  },
+  {
+    "revision": "5de4790c58a8e0d69a96",
+    "url": "/lszyszka/login-form.git/static/css/main.267afeb9.chunk.css"
+  },
+  {
+    "revision": "f0f5481770c73a2bebdc",
+    "url": "/lszyszka/login-form.git/static/css/1.b31323a1.chunk.css"
+  },
+  {
+    "revision": "b286ddda77bdfc828234e4ef02d36331",
+    "url": "/lszyszka/login-form.git/index.html"
+  }
+];
